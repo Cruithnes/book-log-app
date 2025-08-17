d5e6d988b0b4520da1c9caf44f6f303647982679
Cruithnes <emremert959@gmail.com>
2025-08-18 00:07:48 +0300

    fixes

--- middleware.ts
+++ middleware.ts
@@ -6,7 +6,6 @@ import { isAdmin } from "@/lib/utils";
 
 export async function middleware(req: any) {
     const token = await getToken({ req, secret: process.env.AUTH_SECRET });
-    const { pathname } = req.nextUrl
 
     if (!token) {
         return NextResponse.redirect(new URL("/", req.url));
@@ -17,5 +16,6 @@ export async function middleware(req: any) {
 
 export const config = {
     //   matcher: ["/books/new/:path*", "/books/:path/quote"], // korunan rotalar
-    matcher: ["/books/new", "/books/:path*/edit", "/books/:path*/edit/quote"],
+    // matcher: ["/books/new", "/books/:path*/edit", "/books/:path*/edit/quote"],
+    matcher: ["/protected"],
 };
